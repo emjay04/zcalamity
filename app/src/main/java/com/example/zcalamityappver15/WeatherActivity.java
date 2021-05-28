07f6da1349a2aa9487be22fc909557539dbd712a
emjay04 <61629925+emjay04@users.noreply.github.com>
2021-05-28 13:55:28 +0800

    weather app done

--- app/src/main/java/com/example/zcalamityappver15/WeatherActivity.java
+++ app/src/main/java/com/example/zcalamityappver15/WeatherActivity.java
@@ -1,11 +1,15 @@
 package com.example.zcalamityappver15;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,10 +21,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Calendar;
 
 
 public class WeatherActivity extends AppCompatActivity {
@@ -28,44 +35,117 @@ public class WeatherActivity extends AppCompatActivity {
     EditText text;
     Button button;
     ImageView imageView;
-    TextView location_yt, brngy_yt, mun_yt, type_yt;
-    String username = "baguio.alyssa";
-    String password = "Ldw3M8Sr";
+    TextView country_yt, city_yt, temp_yt, date_yt, longitude, latitude, humidity, sunrise, sunset, pressure, wind;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        try{
-            String url = "https://philsensors.asti.dost.gov.ph/api/station";
-            HttpURLConnection conn = (HttpURLConnection) weburl.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-// snippet begins
-            conn.setRequestProperty("Authorization",
-                    "Basic " + Base64.getEncoder().encodeToString(
-                            (username + ":" + password).getBytes()
-                    )
-            );
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-            //print result
-            System.out.println(response.toString());
-            // below code converts the json response to json object and reads each values
-            JSONObject jsonObj = new JSONObject(response.toString());
-            System.out.println(jsonObj.getString("station_id"));
-// snippet ends
+    text=findViewById(R.id.cityname);
+    button=findViewById(R.id.btn);
+    imageView=findViewById(R.id.imageView3);
+    country_yt=findViewById(R.id.country);
+    city_yt=findViewById(R.id.city);
+    temp_yt=findViewById(R.id.temp);
+    date_yt=findViewById(R.id.date);
 
-        }catch(Exception e){
-//do exception handling here
+    longitude=findViewById(R.id.Longitude);
+    latitude=findViewById(R.id.Latitude);
+    humidity=findViewById(R.id.Humidity);
+    sunrise=findViewById(R.id.Sunrise);
+    sunset=findViewById(R.id.Sunset);
+    pressure=findViewById(R.id.Pressure);
+    wind=findViewById(R.id.Wind);
 
+
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            findWeather();
         }
+    });
+
+    }
+
+    public void findWeather(){
+        String city = text.getText().toString();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=31765415ed251c53ae90ff61d828688b";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(String response) {
+                //calling API
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                //find country
+                    JSONObject object1 = jsonObject.getJSONObject("sys");
+                    String country_find = object1.getString("country");
+                    country_yt.setText(country_find+"   :");
+                //find city
+                    String city_find = jsonObject.getString("name");
+                    city_yt.setText(city_find);
+                //find temperature
+                    JSONObject object2 = jsonObject.getJSONObject("main");
+                    double temp_find = object2.getDouble("temp");
+                    temp_yt.setText(temp_find+"°C");
+                //find date & time
+                    Calendar calendar = Calendar.getInstance();
+                    SimpleDateFormat std = new SimpleDateFormat("dd/MM/yyyy \nHH:mm:ss");
+                    String date = std.format(calendar.getTime());
+                    date_yt.setText(date);
+                //find image icon
+                    JSONArray jsonArray = jsonObject.getJSONArray("weather");
+                    JSONObject obj = jsonArray.getJSONObject(0);
+                    String icon = obj.getString("icon");
+                    Picasso.get().load("http://openweathermap.org/img/wn/"+icon+"@2x.png").into(imageView);
+                //find latitude
+                    JSONObject object = jsonObject.getJSONObject("coord");
+                    double lat_find = object.getDouble("lat");
+                    latitude.setText(lat_find+" °N");
+
+                //find longitude
+                    JSONObject object3 = jsonObject.getJSONObject("coord");
+                    double long_find = object3.getDouble("lon");
+                    longitude.setText(long_find+" °E");
+                //find humidity
+                    JSONObject object4 = jsonObject.getJSONObject("main");
+                    int humidity_find = object4.getInt("humidity");
+                    humidity.setText(humidity_find+" %");
+                //find sunrise
+                    JSONObject object5 = jsonObject.getJSONObject("sys");
+                    String sunrise_find = object5.getString("sunrise");
+                    sunrise.setText(sunrise_find+"");
+                //find sunset
+                    JSONObject object6 = jsonObject.getJSONObject("sys");
+                    String sunset_find = object6.getString("sunset");
+                    sunset.setText(sunset_find+"");
+                //find pressure
+                    JSONObject object7 = jsonObject.getJSONObject("main");
+                    String pressure_find = object7.getString("pressure");
+                    pressure.setText(pressure_find+" hPa");
+                //find wind
+                    JSONObject object8 = jsonObject.getJSONObject("wind");
+                    String wind_find = object8.getString("speed");
+                    wind.setText(wind_find+" km/h");
+
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(WeatherActivity.this,error.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(WeatherActivity.this);
+        requestQueue.add(stringRequest);
     }
 
 }
